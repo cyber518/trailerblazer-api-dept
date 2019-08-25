471d394413d0be9c369b6251f18d237f1e0c59b6
Mert Kuvel <mertkuvel@gmail.com>
2019-08-26 00:07:01 +0300

    Fixed route definiton

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 //Assign routes to paths
 app.use('/search', searchRouter);
-app.use('/api/user', authRRouter);
+app.use('/user', authRRouter);
 
 //Handle errors
 app.use((req, res, next) => {
